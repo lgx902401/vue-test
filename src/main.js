a14f3ce7bae6e0874b37c921a876a02cf4281326
Lam <920065232@qq.com>
2019-07-25 17:55:16 +0800

    Signed-off-by: Lam <920065232@qq.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
+Vue.use(Vant);
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <van-button @touchstart.stop="show = true">
+      弹出默认键盘
+    </van-button>
+
+    <van-number-keyboard
+            :show="show"
+            extra-key="."
+            close-button-text="完成"
+            @blur="show = false"
+            @input="onInput"
+            @delete="onDelete"></van-number-keyboard>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  data(){
+    return{
+      show:true
+    }
+  },
+  methods: {
+    onInput(value) {
+      Toast(value);
+    },
+    onDelete() {
+      Toast('删除');
+    }
   }
 }
 </script>
